d40e1cb9e469408e98d6f326be0146d06b435dd0
Edward Minaya <edward.minaya@bitboxonline.com.do>
2025-02-07 14:54:49 -0400

    redux toolkit added

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,15 @@ import { createRoot } from "react-dom/client";
 import { StudyManager } from "./StudyManager";
 import "./style.css";
 import { BrowserRouter } from "react-router";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <Provider store={store}>
     <BrowserRouter>
       <StudyManager />
     </BrowserRouter>
+    </Provider>
   </StrictMode>
 );
